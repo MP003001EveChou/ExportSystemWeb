b83f4a1c1ace0af2be96e68eddfa13a708c871ee
MP003001EveChou <126844391+MP003001EveChou@users.noreply.github.com>
2023-03-03 13:42:05 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+with sync_playwright() as p:
+    
+    browser = p.chromium.launch(channel="chrome")
+    page = browser.new_page()
+    #page.goto("http://playwright.dev")
+    page.goto("http://www.google.com")
+    print(page.title())
+    browser.close()
+
